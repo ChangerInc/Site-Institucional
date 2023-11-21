912bf0a6f0763e9ba35d66f7b553e9e75da5b11a
MateusjfSilva <mateus.jsilva@sptech.school>
2023-11-20 23:58:18 -0300

    Interações do ArquivoBox

--- src/components/ArquivoBox.jsx
+++ src/components/ArquivoBox.jsx
@@ -1,23 +1,29 @@
+
 import React, { useState } from "react";
 import { vertopal } from "../api";
 import { saveAs } from "file-saver";
 import '../styles/arquivo-box.css'
-import { json } from "react-router-dom";
 
 function ArquivoBox() {
+  const formData = new FormData();
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
   const [extensao, setExtensao] = useState('');
-  const formData = new FormData();
+  const [isConverted, setIsConverted] = useState(false);
+  const [isSelectVisible, setIsSelectVisible] = useState(true);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
+    setFileName(event.target.files[0].name);
 
     handleFileUpload(event.target.files[0]);
   };
 
   const handleSelectChange = (event) => {
     setExtensao(event.target.value);
+    if (file) {
+      setIsSelectVisible(false);
+    }
   }
 
   const handleFileUpload = async (file) => {
@@ -41,6 +47,7 @@ function ArquivoBox() {
         formData
       );
       console.log(response.data);
+      setIsConverted(true);
     }
     catch (error) {
       console.error(error);
@@ -69,28 +76,55 @@ function ArquivoBox() {
     }
   };
 
+  const handleClick = () => {
+    if (isConverted) {
+      // Lógica de download aqui
+      handleFileDownload();
+      console.log("Download");
+    } else {
+      // Lógica de conversão aqui
+      handleFileConversion();
+      console.log("Converter");
+    }
+  };
+
   return (
 
     <div className="caixa_de_conversao">
       <div className="container_input_e_select">
-        <label for="file_upload" class="custom-file-upload-label">
-          <b class="bold_selecionar_arquivo">Selecionar Arquivo</b>
-        </label>
-        <input id="file_upload" type="file" onChange={handleFileChange} />
-        <select value={extensao} onChange={handleSelectChange}>
-          <option value="0" selected></option>
-          <option value="pdf" >pdf</option>
-          <option value="png">png</option>
-          <option value="docx">docx</option>
-          <option value="jpeg">jpeg</option>
-          <option value="txt">txt</option>
-        </select>
+        {isSelectVisible && !file && (
+          <>
+            <label for="file_upload" class="custom-file-upload-label">
+              <b class="bold_selecionar_arquivo">{file == null ? "Selecionar Arquivo" : <span>{fileName}</span>}</b>
+            </label>
+            <input id="file_upload" type="file" onChange={handleFileChange} />
+            <select id="select_extensao"
+              value={extensao}
+              onChange={handleSelectChange}
+              style={{
+                backgroundImage: extensao ? 'none' : 'url("/src/assets/SetaCombo.png")',
+              }}>
+              <option value="" selected></option>
+              <option value="pdf" >pdf</option>
+              <option value="png">png</option>
+              <option value="docx">docx</option>
+              <option value="jpeg">jpeg</option>
+              <option value="txt">txt</option>
+            </select>
+          </>
+        )}
+        {file && extensao && !isSelectVisible && (
+          <>
+            <button onClick={handleClick}>
+              {isConverted ? "Download" : "Converter"}
+            </button>
+            <button onClick={() => setIsSelectVisible(true)}>Alterar extensão<br /><span>Atual: {extensao}</span></button>
+          </>
+        )}
       </div>
       <span className="texto_box_informacao">
         Solte os arquivos aqui. 100 MB tamanho máximo do ficheiro ou <a href="">Registrar-se</a>.
       </span>
-      {/*       <button onClick={handleFileConversion}>Converter</button>
-      <button onClick={handleFileDownload}>Baixar</button> */}
     </div>
   );
 }

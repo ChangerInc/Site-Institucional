a3af6bd90a1b37d579e8b1722dbef753841b22d2
MateusjfSilva <mateus.jsilva@sptech.school>
2023-11-24 23:11:58 -0300

    Animação de conversão

--- src/components/ArquivoBox.jsx
+++ src/components/ArquivoBox.jsx
@@ -1,30 +1,24 @@
-
-import React, { useState } from "react";
-import { ClipLoader } from 'react-spinners';
-import { css } from '@emotion/react';
+import React, { useState, useRef } from "react";
 import { vertopal } from "../api";
 import { saveAs } from "file-saver";
 import { Link } from 'react-router-dom';
-import './styles/arquivo-box.css'
+import { Player } from '@lottiefiles/react-lottie-player';
+import './styles/arquivo-box.css';
 
 function ArquivoBox() {
-  const formData = new FormData();
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
   const [extensao, setExtensao] = useState('');
   const [isConverted, setIsConverted] = useState(false);
   const [isSelectVisible, setIsSelectVisible] = useState(true);
-  const [loading, setLoading] = useState(false);
-
-  function handleLoading() {
-    setLoading(true);
-  }
+  const [isPlaying, setIsPlaying] = useState(false);
+  const lottieRef = useRef(null);
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-    setFileName(event.target.files[0].name);
-
-    handleFileUpload(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setFileName(selectedFile.name);
+    handleFileUpload(selectedFile);
   };
 
   const handleSelectChange = (event) => {
@@ -34,93 +28,100 @@ function ArquivoBox() {
     }
   }
 
-  const handleFileUpload = async (file) => {
-    if (file) {
-      formData.append("file", file);
-      try {
-        const response = await vertopal.post("/enviar",
-          formData
-        );
-        const element = document.getElementById('arquivo-box');
-        if (response.status == 200) {
-          element.style.border = '3px solid #8eef80';
-          element.style.width = 'calc(290px - 6px)';
-          element.style.height = 'calc(60px - 6px)';
-          if (extensao != '') {
-            setIsSelectVisible(false);
+  const handleFileUpload = (uploadedFile) => {
+    if (uploadedFile) {
+      const formData = new FormData();
+      formData.append("file", uploadedFile);
+
+      vertopal
+        .post("/enviar", formData)
+        .then((response) => {
+          const element = document.getElementById('arquivo-box');
+          if (response.status === 200) {
+            element.style.border = '3px solid #8eef80';
+            element.style.width = 'calc(290px - 6px)';
+            element.style.height = 'calc(60px - 6px)';
+            if (extensao !== '') {
+              setIsSelectVisible(false);
+            }
           }
-        }
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
   };
 
-  const handleFileConversion = async () => {
-    formData.append("extensao", extensao);
-    try {
-      const response = await vertopal.post("/converter",
-        formData
-      );
-      console.log(response.data);
-      if (response.status == 200) {
-        setIsConverted(true);
-        setLoading(false);
-      }
-    }
-    catch (error) {
-      console.error(error);
-    }
-    finally {
-      handleUrl();
-    }
-  }
+  const handleFileConversion = () => {
+    const conversionFormData = new FormData();
+    conversionFormData.append("extensao", extensao);
 
-  const handleUrl = async () => {
-    try {
-      const response = await vertopal.get("/url");
-      setFileName(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+    vertopal
+      .post("/converter", conversionFormData)
+      .then((response) => {
+        console.log(response.data);
+        if (response.status === 200) {
+          setIsConverted(true);
+          setIsPlaying(false);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        document.getElementById('consersion-download').style.display = 'flex';
+        handleUrl();
+      });
+  };
 
-  const handleFileDownload = async () => {
-    try {
-      const response = await vertopal.get("/baixar", { responseType: "blob" });
-      saveAs(response.data, `${fileName}`);
-      console.log(response.data);
-      if (response.status = 200) {
-        setLoading(false);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setFile(null);
-      setFileName('');
-      setIsSelectVisible(true);
-      setExtensao('');
-    }
+  const handleUrl = () => {
+    vertopal
+      .get("/url")
+      .then((response) => {
+        setFileName(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleFileDownload = () => {
+    vertopal
+      .get("/baixar", { responseType: "blob" })
+      .then((response) => {
+        saveAs(response.data, `${fileName}`);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        resetState();
+      });
+  };
+
+  const resetState = () => {
+    setFile(null);
+    setFileName('');
+    setIsSelectVisible(true);
+    setExtensao('');
   };
 
   const handleClick = () => {
-    handleLoading();
-    const btnExtensao = document.getElementById('alterar-extensao');
-    btnExtensao.style.display = 'none';
+    document.getElementById('alterar-extensao').style.display = 'none';
     if (isConverted) {
-      // Lógica de download aqui
       handleFileDownload();
       console.log("Download");
     } else {
-      // Lógica de conversão aqui
+      setIsPlaying(true);
+      document.getElementById('consersion-download').style.display = 'none';
       handleFileConversion();
       console.log("Converter");
     }
   };
 
   return (
-
     <div className="caixa_de_conversao">
       <div className="container_input_e_select">
         {(isSelectVisible || !file) && (
@@ -129,14 +130,16 @@ function ArquivoBox() {
               <b className="bold_selecionar_arquivo">{file == null ? "Selecionar Arquivo" : <span>{fileName}</span>}</b>
             </label>
             <input id="file_upload" type="file" onChange={handleFileChange} />
-            <select id="select_extensao"
+            <select
+              id="select_extensao"
               value={extensao}
               onChange={handleSelectChange}
               style={{
                 backgroundImage: extensao ? 'none' : 'url("/src/assets/SetaCombo.png")',
-              }}>
+              }}
+            >
               <option defaultValue={0}></option>
-              <option value="pdf" >PDF</option>
+              <option value="pdf">PDF</option>
               <option value="png">PNG</option>
               <option value="docx">DOCX</option>
               <option value="jpeg">JPEG</option>
@@ -146,21 +149,29 @@ function ArquivoBox() {
         )}
         {!isSelectVisible && (
           <>
-            <button id="consersion-download" onClick={handleClick} disabled={loading}>
+            <button id="consersion-download" onClick={handleClick}>
               {isConverted ? "Download" : "Converter"}
-              {loading ? (
-                <ClipLoader color="#ffffff" loading={loading} css={css`margin-right: 5px;`} size={15} />
-              ) : (
-                ''
-              )}
             </button>
-            <button id="alterar-extensao" onClick={() => setIsSelectVisible(true)}>Alterar extensão<span>Atual: {extensao}</span></button>
+            <button id="alterar-extensao" onClick={() => setIsSelectVisible(true)}>
+              Alterar extensão<span>Atual: {extensao}</span>
+            </button>
+            {isPlaying && (
+              <Player
+                lottieRef={lottieRef}
+                autoplay={true}
+                loop={true}
+                src="https://lottie.host/83fa29a2-5510-464f-987e-05cb2fbb1fc5/FnH2MtYORH.json"
+                style={{ height: '150px', width: '150px' }}
+              ></Player>
+            )}
           </>
         )}
       </div>
-      <span className="texto_box_informacao">
-        Solte os arquivos aqui. 100 MB tamanho máximo do ficheiro ou <Link href="" to="/cadastro">Registrar-se</Link>.
-      </span>
+      {isSelectVisible && (
+        <span className="texto_box_informacao">
+          Solte os arquivos aqui. 100 MB tamanho máximo do ficheiro ou <Link href="" to="/cadastro">Registrar-se</Link>.
+        </span>
+      )}
     </div>
   );
 }

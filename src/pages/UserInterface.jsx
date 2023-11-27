5dd6151ab3d0cef103a266374fc86b3c617f02b7
NataSFSilva <nata.ssilva@sptech.school>
2023-11-27 13:14:27 -0300

    Add upload de arquivos

--- src/pages/UserInterface.jsx
+++ src/pages/UserInterface.jsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import './styles-pages/user-interface.css'
 import { format } from 'date-fns';
+import { usuario } from "../api";
 import aviIcon from '../assets/icones/avi.png';
 import docxIcon from '../assets/icones/doc.png';
 import zipIcon from '../assets/icones/zip.png';
@@ -20,10 +21,40 @@ import txtIcon from '../assets/icones/txt.png';
 import xlsIcon from '../assets/icones/xls.png';
 
 const UserInterface = () => {
-  const navigate = useNavigate();
   const username = sessionStorage?.getItem('nome');
   const id = sessionStorage?.getItem('id');
   const [historico, setHistorico] = useState([]);
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState('');
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setFileName(selectedFile.name);
+    handleFileUpload(selectedFile);
+  };
+
+  const handleFileUpload = (uploadedFile) => {
+    if (uploadedFile) {
+      const formData = new FormData();
+      formData.append("file", uploadedFile);
+
+      console.log(sessionStorage.getItem("id"));
+
+      usuario
+        .post(`/upload/${sessionStorage.getItem("id")}`, formData)
+        .then((response) => {
+          const element = document.getElementById('arquivo-box');
+          if (response.status === 200) {
+
+          }
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
 
   useEffect(() => {
     const fetchHistorico = async () => {
@@ -65,6 +96,10 @@ const UserInterface = () => {
             <p>Obrigado por entrar no sistema da</p><b>CHANGER</b><div className='ponto'>.</div>
           </div>
         </div>
+        <label id="arquivo-box" htmlFor="file_upload" className="custom-file-upload-label">
+          <b className="bold_selecionar_arquivo">{file == null ? "Salvar arquivo" : <span>{fileName}</span>}</b>
+        </label>
+        <input id="file_upload" type="file" onChange={handleFileChange} />
       </div>
       <div className="boxPai">
         <div className="box">

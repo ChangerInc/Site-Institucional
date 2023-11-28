d62b1a2d5224f2b344c85fa302425883db56b497
NataSFSilva <nata.ssilva@sptech.school>
2023-11-28 14:58:47 -0300

    Histórico em componente

---
+++ src/components/Historico.jsx
@@ -0,0 +1,52 @@
+import React, { props } from 'react'
+import aviIcon from '../assets/icones/avi.png';
+import docxIcon from '../assets/icones/doc.png';
+import zipIcon from '../assets/icones/zip.png';
+import gifIcon from '../assets/icones/gif.png';
+import jpegIcon from '../assets/icones/jpeg.png';
+import mp3Icon from '../assets/icones/mp3.png';
+import pdfIcon from '../assets/icones/pdf.png';
+import pngIcon from '../assets/icones/png.png';
+import pptIcon from '../assets/icones/ppt.png';
+import psdIcon from '../assets/icones/psd.png';
+import svgIcon from '../assets/icones/svg.png';
+import txtIcon from '../assets/icones/txt.png';
+import xlsIcon from '../assets/icones/xls.png';
+
+const Historico = (props) => {
+    const iconPaths = {
+        avi: aviIcon,
+        docx: docxIcon,
+        zip: zipIcon,
+        gif: gifIcon,
+        jpeg: jpegIcon,
+        mp3: mp3Icon,
+        pdf: pdfIcon,
+        png: pngIcon,
+        ppt: pptIcon,
+        psd: psdIcon,
+        svg: svgIcon,
+        txt: txtIcon,
+        xls: xlsIcon,
+    };
+
+    return (
+        <div className="historico">
+            <div className="espacamento">
+                <img src={iconPaths[props.extensaoAtual]} alt={props.extensaoAtual} />
+                <p>{props.nome}</p>
+            </div>
+            <div className="espacamento">
+                <p>{props.dataConversao}</p>
+            </div>
+            <div className="espacamento">
+                <p>{props.extensaoInicial}</p>
+            </div>
+            <div className="espacamento">
+                <p>{props.extensaoAtual}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Historico;
--- src/pages/UserInterface.jsx
+++ src/pages/UserInterface.jsx
@@ -1,28 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import axios from "axios";
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Historico from '../components/Historico';
 import './styles-pages/user-interface.css'
 import { format } from 'date-fns';
-import { usuario } from "../api";
-import aviIcon from '../assets/icones/avi.png';
-import docxIcon from '../assets/icones/doc.png';
-import zipIcon from '../assets/icones/zip.png';
-import gifIcon from '../assets/icones/gif.png';
-import jpegIcon from '../assets/icones/jpeg.png';
-import mp3Icon from '../assets/icones/mp3.png';
-import pdfIcon from '../assets/icones/pdf.png';
-import pngIcon from '../assets/icones/png.png';
-import pptIcon from '../assets/icones/ppt.png';
-import psdIcon from '../assets/icones/psd.png';
-import svgIcon from '../assets/icones/svg.png';
-import txtIcon from '../assets/icones/txt.png';
-import xlsIcon from '../assets/icones/xls.png';
+import { usuario, historico } from "../api";
+
 
 const UserInterface = () => {
   const username = sessionStorage?.getItem('nome');
   const id = sessionStorage?.getItem('id');
-  const [historico, setHistorico] = useState([]);
+  const [historicoArq, setHistorico] = useState([]);
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
 
@@ -42,56 +30,40 @@ const UserInterface = () => {
 
       usuario
         .post(`/upload/${sessionStorage.getItem("id")}`, formData)
-        .then((response) => {
-          const element = document.getElementById('arquivo-box');
+        .then(response => {
           if (response.status === 200) {
-            
+            console.log(response.data);            
           }
-          console.log(response.data);
         })
         .catch((error) => {
           console.error(error);
         })
         .finally(() => {
           setFileName("Salvar arquivo");
+          fetchHistorico();
         });
     }
   };
 
   
-  useEffect(() => {
-    const fetchHistorico = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8080/historico-conversao/usuario/${id}`);
+  const fetchHistorico = async () => {
+    historico.get(`/usuario/${id}`)
+      .then(response => {
         if (response.status === 204) {
-          
+          console.warn("Não há arquivos no histórico");
         } else {
-          
-        }
-        console.log(response.data);
-        setHistorico(response.data);
-      } catch (error) {
+          console.log(response.data);
+          setHistorico(response.data);          
+        }})
+      .catch(error => {
         console.log(error);
-      }
-    };
+      });
+  }
+  
+
+  useEffect(() => {
     fetchHistorico();
   }, []);
-
-  const iconPaths = {
-    avi: aviIcon,
-    docx: docxIcon,
-    zip: zipIcon,
-    gif: gifIcon,
-    jpeg: jpegIcon,
-    mp3: mp3Icon,
-    pdf: pdfIcon,
-    png: pngIcon,
-    ppt: pptIcon,
-    psd: psdIcon,
-    svg: svgIcon,
-    txt: txtIcon,
-    xls: xlsIcon,
-  };
 
   return (
     <>
@@ -124,7 +96,7 @@ const UserInterface = () => {
               <b>Extensão atual</b>
             </div>
           </div>
-          {(historico.length == 0) ? (
+          {(historicoArq.length == 0) ? (
             <>
               <div className='msgSemArq'>
                 <b>
@@ -133,22 +105,13 @@ const UserInterface = () => {
               </div>
             </>
           ) : (
-            historico.map((hist, index) => (
-              <div className="historico" key={index}>
-                <div className="espacamento">
-                  <img src={iconPaths[hist.extensaoAtual]} alt={hist.extensaoAtual} />
-                  <p>{hist.nome}</p>
-                </div>
-                <div>
-                  <p>{format(new Date(hist.dataConversao), 'dd/MM/yy HH:mm')}</p>
-                </div>
-                <div>
-                  <p>{hist.extensaoInicial}</p>
-                </div>
-                <div>
-                  <p>{hist.extensaoAtual}</p>
-                </div>
-              </div>
+            historicoArq.map(hist => (
+              <Historico
+                nome = {hist.nome}
+                extensaoAtual = {hist.extensaoAtual}
+                dataConversao = {format(new Date(hist.dataConversao), 'dd/MM/yy HH:mm')}
+                extensaoInicial = {hist.extensaoInicial}
+              />
             ))
           )}
         </div>
